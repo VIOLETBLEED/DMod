b6a7a7511ae620789ce185a286adc09b2b082d9a
Cyn <bh3@crepe.moe>
2024-03-16 12:58:30 +0000

    my glitcha what

--- src/main/java/me/dmod/mixins/MixinItemStack.java
+++ src/main/java/me/dmod/mixins/MixinItemStack.java
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(value = ItemStack.class, remap = false)
+@Mixin(value = ItemStack.class)
 public abstract class MixinItemStack {
     @Shadow
     private RegistryDelegate<Item> delegate;
